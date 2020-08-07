e47914707bc01c9420e20147859d605e2e7d8e33
John-Sang-Pi <superkiller498@gmail.com>
2020-08-07 18:45:19 +0630

    Updates

---
+++ precache-manifest.2c76aaeeea40c71f47d160a158e172eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3943dd7f3f692fe35f3051d5571d2b88",
+    "url": "/react-tests.git/index.html"
+  },
+  {
+    "revision": "d74a5c27994ea258ce24",
+    "url": "/react-tests.git/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "12f3633d0dc4c141395e",
+    "url": "/react-tests.git/static/js/2.5bd1d3b4.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/react-tests.git/static/js/2.5bd1d3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74a5c27994ea258ce24",
+    "url": "/react-tests.git/static/js/main.a09e4ca3.chunk.js"
+  },
+  {
+    "revision": "61c32fc7f7f8d8cbc934",
+    "url": "/react-tests.git/static/js/runtime-main.4713a1d4.js"
+  }
+]);
