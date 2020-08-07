540c424ec6b35500777e30295b97549caf64f0fb
John-Sang-Pi <superkiller498@gmail.com>
2020-08-07 23:30:19 +0630

    Checkbox Updated

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ import SearchForm from './SearchForm';
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
+  const [checked, setChecked] = useState(false);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
   const handleParamChange = e => {
     const param = e.target.name;
-    const value = e.target.value;
+    let value = e.target.value;
+    if(param === "full_time") {
+      // console.log(e.target.checked);
+      value = e.target.checked;
+      setChecked(value);
+    } 
     setPage(1);
     setParams(prevParams => {
       return { ...prevParams, [param]: value}
@@ -27,7 +33,7 @@ function App() {
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
-      <SearchForm params={params} onParamChange={handleParamChange} />
+      <SearchForm params={params} onParamChange={handleParamChange} checked={checked} />
       <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1 className="mb-4">Loading...</h1>}
       {error && <h1 className="mb-4">Error! Try refreshing.</h1>}
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Form, Col } from 'react-bootstrap';
 
-export default function SearchForm({ params, onParamChange }) {
+export default function SearchForm({ params, onParamChange, checked }) {
     return (
         <Form className="mb-4">
             <Form.Row className="align-items-end">
@@ -15,7 +15,7 @@ export default function SearchForm({ params, onParamChange }) {
                     <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
                 </Form.Group>
                 <Form.Group as={Col} xs="auto" className="ml-2">
-                    <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full_time" label="Only Full Time" type="checkbox" className="mb-2" />
+                    <Form.Check onChange={onParamChange} value={params.full_time} defaultChecked={checked} name="full_time" id="full_time" label="Full Time Only" type="checkbox" className="mb-2" />
                 </Form.Group>
             </Form.Row>
         </Form>
